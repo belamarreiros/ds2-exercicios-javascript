9201a03ffc5c92646752c6b7c34d408c83cc13ce
Izabela Marreiros <belamarreiros@outlook.com>
2026-08-19 21:25:21 -0300

    feat: pratica variaveis e tipos de dados

---
+++ js/exercicio02.js
@@ -0,0 +1,21 @@
+const nome = "Izabela Marreiros";
+let idade = 25;
+const cidade = "Mairinque";
+const matriculado = true;
+const nota = 9.0;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+console.log(matriculado);
+console.log(nota);
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof cidade);
+console.log(typeof matriculado);
+console.log(typeof nota);
+
+// As variáveis foram declaradas com const porque seus valores não precisam ser alterados.
+
+// A idade foi declarada com let porque seu valor pode ser alterado durante a execução do programa.
